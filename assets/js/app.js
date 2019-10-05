495fa7d5d20a1013fac70c2d0f1fd73bf881ef3b
r0b0_s1av3 <55931329+r0b0-s1av3@users.noreply.github.com>
2019-10-04 23:32:46 -0500

    Removed unused 'getRandomInt' function
    This function was used in an earlier version, replaced by the current shorthand since the function will always deal with the same min and max (0, 255).

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,6 @@ function contrast(r, g, b) {
   return ((299 * r) + (587 * g) + (114 * b)) / 1000;
 }
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function generate() {
   $(".row").empty();
   for (var i = 0; i < 6; i++) {
