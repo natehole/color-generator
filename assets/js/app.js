803ae2af7a0aeb14b81339f7edac4b86aa4a424d
Robert Harvey <robert@efellemedia.com>
2019-10-04 22:25:48 -0700

    Refactor

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,64 @@
-function contrast(r, g, b) {
-  return ((299 * r) + (587 * g) + (114 * b)) / 1000;
-}
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function generate() {
-  $(".row").empty();
-  for (var i = 0; i < 6; i++) {
-    var r = Math.floor(Math.random() * 255);
-    var g = Math.floor(Math.random() * 255);
-    var b = Math.floor(Math.random() * 255);
-    var color = r.toString().concat(", ", g, ", ", b);
-    var contrastResult = contrast(r, g, b);
-    var finalColor = (contrastResult < 123) ? "white" : "black";
-    $(".row").append("<div class='col-md-2' style='background-color: rgb(" + color + ");color: " + finalColor + "'><p class='color-text'>" + color + "</p></div>");
-  }
+  $('.color__column').each(function(index, column){
+    let rgb = getRGB();
+    $(column).find('.color__rgb').text(`(${rgb})`);
+    $(column).find('.color__hex').text('#' + getHex(rgb));
+    $(column).find('.color__cmyk').text(`(${getCMYK(rgb)})`);
+    $(column).css('background-color', `rgb(${rgb})`);
+    if (getContrast(rgb) < 123) { $(column).addClass('text-white'); }
+  })
 }
 
 function switchTheme() {
-  var background = $("body").css("background-color");
-  if (background == "rgb(0, 0, 0)") {
-    $(".fa-moon").removeClass("far");
-    $(".fa-moon").addClass("fas");
-    $("body").css("background-color", "rgb(255,255,255)");
-  } else {
-    $(".fa-moon").removeClass("fas");
-    $(".fa-moon").addClass("far");
-    $("body").css("background-color", "rgb(0,0,0)");
-  }
+  $('body').toggleClass('bg-dark')
+  $('.fa-moon').toggleClass('d-none')
+  $('.fa-sun').toggleClass('d-none')
+}
+
+function getRGB(){
+  var r = Math.floor(Math.random() * 255);
+  var g = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+  return [r, g, b];
+}
+
+function getHex(rgb) {
+  var hex = '';
+  rgb.forEach(function(val){
+    var hexPartial = Number(val).toString(16);
+    if (hexPartial.length < 2) {
+         hexPartial = "0" + hexPartial;
+    }
+    hex = hex + hexPartial;
+  })
+  return hex;
+}
+
+function getCMYK (rgb) {
+ var finalK = 0;
+
+ var r = rgb[0];
+ var g = rgb[1];
+ var b = rgb[2];
+
+ if (r==0 && g==0 && b==0) {
+  finalK = 1;
+  return [0,0,0,1];
+ }
+
+ var finalC = 1 - (r/255);
+ var finalM = 1 - (g/255);
+ var finalY = 1 - (b/255);
+
+ var minCMY = Math.min(finalC, Math.min(finalM,finalY));
+ finalC = Math.trunc(((finalC - minCMY) / (1 - minCMY)) * 100);
+ finalM = Math.trunc(((finalM - minCMY) / (1 - minCMY)) * 100);
+ finalY = Math.trunc(((finalY - minCMY) / (1 - minCMY)) * 100);
+ finalK = Math.trunc(minCMY * 100);
+
+ return (finalC + ',' + finalM + ',' + finalY + ',' + finalK);
+}
+
+function getContrast(rgb) {
+  return ((299 * rgb[0]) + (587 * rgb[1]) + (114 * rgb[2])) / 1000;
 }
